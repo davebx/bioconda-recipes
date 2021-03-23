8064da3777533b659b37bc8decab01ea1d006e21
Dave Bouvier <dave@bx.psu.edu>
2021-03-23 18:19:58 -0400

    Preliminary PretextView recipe.

---
+++ recipes/pretextview/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p $PREFIX/bin
+
+chmod +x install
+
+export CMAKE_PREFIX_PATH=$BUILD_PREFIX
+
+./install
+
+shopt -s extglob
+for file in $(compgen -G "Pretext!(*cpp)"); do
+    chmod +x $file;
+    mv $file $PREFIX/bin/
+done
